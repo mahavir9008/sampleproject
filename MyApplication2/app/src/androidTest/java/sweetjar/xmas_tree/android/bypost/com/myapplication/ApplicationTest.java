9ac4ceaa5f64e3865cac490de475820abe601759
Shubhendu Sharma <shubhendusharma@Shubhendus-Mac-mini-2.local>
2014-11-21 16:53:58 +0530

    initial commit

---
+++ MyApplication2/app/src/androidTest/java/sweetjar/xmas_tree/android/bypost/com/myapplication/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sweetjar.xmas_tree.android.bypost.com.myapplication;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
